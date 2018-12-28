d10df943e1f088283cb57529c24cf050c38fb4fe
willianbello <willianbello.s@gmail.com>
2018-12-28 05:31:37 -0200

    criando static files e referenciando css

--- server.py
+++ server.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 #função que retorna o html salvo na pasta templates
 def index():
-        return render_template('index.html')
+        return render_template('login.html')
 
 #inicia o servidor
 if __name__ == "__main__":
     app.run()
+
+url_for('static', filename='bootstrap/css/bootstrap.min.css')
 
