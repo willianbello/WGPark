aeda90085e692e1b318bb5104c9a0f3aa2569ab8
willianbello <willianbello.s@gmail.com>
2018-12-29 05:34:54 -0200

    colocando a função que exibe hora atual

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,9 +1,35 @@
 $(document).ready(function() {
 
-    var link = $('#linkHistorico');
-    var subir = $('#botaoSubir');
+    function iniciar() {
+        setInterval(mostrarData, 1000);
+    }
+
+    function mostrarData() {
+
+        var data = new Date();
+
+        var dia = data.getDate(); // 1-31
+        var mes = data.getMonth(); // 0-11 (zero=janeiro)
+        var ano4 = data.getFullYear(); // 4 dígitos
+
+        var hora = data.getHours(); // 0-23
+        var min = data.getMinutes(); // 0-59
+        var seg = data.getSeconds(); // 0-59
+
+        if (hora < 10) hora = "0" + hora
+        if (min < 10) min = "0" + min
+        if (seg < 10) seg = "0" + seg
+
+        var str_data = dia + '/' + (mes + 1) + '/' + ano4;
+        var str_hora = hora + ':' + min + ':' + seg;
+
+        document.getElementById('dia').value = str_data;
+        document.getElementById('hora').value = str_hora;
+        document.getElementById('horario').value = str_hora;
+    }
 
     $(link).click(function() {
+
         var seletor = $(this).attr("href");
         console.log(seletor);
         var posicao = $(seletor).offset().top;
