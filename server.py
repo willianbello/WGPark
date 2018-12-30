98d85b329bb012cca613ab216c915e75c3a6edff
willianbello <willianbello.s@gmail.com>
2018-12-30 02:32:52 -0200

    correção de eventos jQuery e adicionado efeito de blur ao abrir modal

--- server.py
+++ server.py
@@ -12,11 +12,3 @@ def index():
 #inicia o servidor
 if __name__ == "__main__":
     app.run()
-<<<<<<< HEAD
-
-#acesso a url
-url_for('static', filename='bootstrap/css/bootstrap.min.css')
-
-=======
->>>>>>> aeda90085e692e1b318bb5104c9a0f3aa2569ab8
-
--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,32 +1,8 @@
+
 $(document).ready(function() {
 
-    function iniciar() {
-        setInterval(mostrarData, 1000);
-    }
-
-    function mostrarData() {
-
-        var data = new Date();
-
-        var dia = data.getDate(); // 1-31
-        var mes = data.getMonth(); // 0-11 (zero=janeiro)
-        var ano4 = data.getFullYear(); // 4 dígitos
-
-        var hora = data.getHours(); // 0-23
-        var min = data.getMinutes(); // 0-59
-        var seg = data.getSeconds(); // 0-59
-
-        if (hora < 10) hora = "0" + hora
-        if (min < 10) min = "0" + min
-        if (seg < 10) seg = "0" + seg
-
-        var str_data = dia + '/' + (mes + 1) + '/' + ano4;
-        var str_hora = hora + ':' + min + ':' + seg;
-
-        document.getElementById('dia').value = str_data;
-        document.getElementById('hora').value = str_hora;
-        document.getElementById('horario').value = str_hora;
-    }
+    var link = $('#linkHistorico');
+    var subir = $('#botaoSubir');
 
     $(link).click(function() {
 
@@ -54,3 +30,31 @@ $(document).ready(function() {
         }
     });
 });
+
+function iniciar() {
+    setInterval(mostrarData, 1000);
+}
+
+function mostrarData() {
+
+    var data = new Date();
+
+    var dia = data.getDate(); // 1-31
+    var mes = data.getMonth(); // 0-11 (zero=janeiro)
+    var ano4 = data.getFullYear(); // 4 dígitos
+
+    var hora = data.getHours(); // 0-23
+    var min = data.getMinutes(); // 0-59
+    var seg = data.getSeconds(); // 0-59
+
+    if (hora < 10) hora = "0" + hora
+    if (min < 10) min = "0" + min
+    if (seg < 10) seg = "0" + seg
+
+    var str_data = dia + '/' + (mes + 1) + '/' + ano4;
+    var str_hora = hora + ':' + min + ':' + seg;
+
+    document.getElementById('dia').value = str_data;
+    document.getElementById('hora').value = str_hora;
+    document.getElementById('horario').value = str_hora;
+}
