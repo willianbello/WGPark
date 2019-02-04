8620c50439b230cbf41f43508c6e558e20e4e9e3
giovani <giovani.lima@rede.ulbra.br>
2019-02-04 10:22:05 -0200

    Criação da função de login e logout e persistência do login e demais ajustes

--- server.py
+++ server.py
@@ -3,40 +3,64 @@ from flask import *
 from db import *
 from forms import *
 
-global validador = None
+# essa variavel global salva o login e senha para ficar persistindo caso a pessoa já esteja logada, impedindo de acessar novamente
+# outras partes do site, como o /login
+validador = None
 
 # instancia o app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'teste'
 
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-
-    if request.method == 'POST':
-
-        usuario = request.form['usuario']
-        senha = request.form['senha']
-        validador = check_usuario(usuario, senha)
-        
-        if validador != []:
-            return redirect(url_for('index'))
-        else:
-            return redirect(url_for('login'))
-    
-    return render_template('login.html')
-    
+# inicia o site
 @app.route('/')
 def iniciar():
 
+    global validador
+
+    if validador == [] or validador == None:
+        return redirect(url_for('login'))
+    else:
+        return redirect(url_for('index'))
+
+# app para logar no site caso exista um usuario cadastrado no BD
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    global validador
+
+    if validador == [] or validador == None:
+
+        if request.method == 'POST':
+
+            usuario = request.form['usuario']
+            senha = request.form['senha']
+            validador = check_usuario(usuario, senha)
+            
+            if validador != [] or validador != None or validador != Exception:
+                return redirect(url_for('index'))
+            else:
+                return redirect(url_for('login'))
+                
+        return render_template('login.html')
+
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/logout')
+def logout():
+
+    global validador
+
+    validador = None
     return redirect(url_for('login'))
 
-# App que para ir ao endereço da página
+# App que para ir ao index da página
 @app.route('/index', methods=['GET', 'POST'])
-# função que retorna o html salvo na pasta templates
-def index(validador):
+# função que retorna o html index salvo na pasta templates
+def index():
 
-    if validador = True:
+    global validador
+    if validador != [] and validador != None and validador != Exception:
         # requisita o formulario do arquivo forms.py
         formulario = FormularioDeCadastro(request.form)
         formulario2 = FormulariodeExclusao(request.form)
@@ -66,14 +90,14 @@ def index(validador):
 @app.route('/delete', methods=['GET', 'POST'])
 def delete():
 
-    formulario2 = FormulariodeExclusao(request.form)
     placa = None
+    formulario2 = FormulariodeExclusao(request.form)
 
     if request.method == 'POST' and formulario2.validate_on_submit():
 
         placa = request.form['pkcodentr']
         insert_sd(request.form['placacarsd'], request.form['dataentrsd'],
-                  request.form['horaentrsd'], request.form['datasd'], request.form['horasd'])
+        request.form['horaentrsd'], request.form['datasd'], request.form['horasd'])
 
     delete_ent(placa)
     return redirect(url_for('index'))
